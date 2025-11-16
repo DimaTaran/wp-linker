70de198a43a1e1fd36719d095ed55a1394db0623
Dmitry Taran <taran_da@ukr.net>
2025-11-16 23:03:04 +0200

    Add the base route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ServerController;
+use App\Http\Controllers\CdnController;
+use App\Http\Controllers\WpCredentialController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Sites - Resource routes (CRUD)
+Route::resource('sites', SiteController::class);
